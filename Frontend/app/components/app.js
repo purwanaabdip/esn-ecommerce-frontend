060c9c31e567ec5054519deb21ba700b5af0f9ec
Eka Setya Nugraha <ekasetyanugraha@gmail.com>
2017-01-24 20:42:38 +0700

    Bugfix : pushed sidebar

--- Frontend/app/components/app.js
+++ Frontend/app/components/app.js
@@ -5,18 +5,22 @@
 import React from 'react';
 
 import Navigation from './navigation';
+import Sidebar from './sidebar';
 
 export default class App extends React.Component {
     render() {
         return (
-        	<div className="pusher" id="app">
-        		<div id="navigation">
-        			<Navigation />
-        		</div>
-        		<div id="content">
-        			{this.props.children}
-        		</div>
-    		</div>
+          <div>
+            <Sidebar />
+          	<div className="pusher">
+          		<div id="navigation">
+          			<Navigation />
+          		</div>
+          		<div id="content">
+          			{this.props.children}
+          		</div>
+      		  </div>
+          </div>
         )
     }
 }
--- Frontend/app/components/sidebar.js
+++ Frontend/app/components/sidebar.js
@@ -3,17 +3,12 @@
 // Copyright 10/28/2016.
 // ================================
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Link } from 'react-router';
 
 export default class Sidebar extends React.Component {
 
     render() {
         return (
-            <div class="ui right sidebar" id="sidebar">
-            </div>
+            <div className="ui right sidebar"></div>
         )
     }
 }
-
-ReactDOM.render(<Sidebar />, document.body)

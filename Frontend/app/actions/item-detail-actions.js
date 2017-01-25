4f713b1de2e0d55b86ee53dfebaf5cdc75368087
Eka Setya Nugraha <ekasetyanugraha@gmail.com>
2017-01-25 22:18:58 +0700

    Implementing first XHR through axios! (love it)

--- Frontend/app/actions/item-detail-actions.js
+++ Frontend/app/actions/item-detail-actions.js
@@ -2,22 +2,16 @@
 // Created by Eka Setya Nugraha.
 // Copyright 1/21/2017.
 // ================================
-import dispatcher from '../dispatcher'
+import axios from 'axios';
+import dispatcher from '../dispatcher';
 
+// Get individual item
 export function getItem(id) {
-    dispatcher.dispatch({type: 'xhr_start'});
+  dispatcher.dispatch({type: 'xhr_start'});
 	const url = 'http://localhost:3000/item/' + id;
-	var response = {};
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true); // true for asynchronous
-    xhr.send();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-        	let result = JSON.parse(xhr.responseText);
-		    response = result.data;
-        }
-    }
-    xhr.onload = function() {
-		dispatcher.dispatch({type: 'get_item', data: response});
-    }
+	axios.get(url).then(function(response){
+		dispatcher.dispatch({type: 'get_item', data: response.data.data});
+	}).catch(function(error){
+		console.log(error);
+	});
 }

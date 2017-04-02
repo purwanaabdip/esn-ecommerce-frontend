83e8b1f6bc73af3e45c46fe96ac2edef5bb712ad
Eka Setya Nugraha <ekasetyanugraha@gmail.com>
2017-04-02 15:56:06 +0700

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,21 @@ var debug = process.env.NODE_ENV !== "production";
 var webpack = require("webpack");
 
 module.exports = {
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+    watchOptions: { aggregateTimeout: 300, poll: 1000 },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+  },
   context: __dirname + "/app",
   devtool: debug ? "inline-sourcemap" : null,
   entry: {
     app: "./index.js"
-},
+  },
   module: {
     loaders: [
       {
